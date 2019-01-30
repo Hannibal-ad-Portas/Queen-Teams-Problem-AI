eb066d5875ed386e9d470a906bcbd529f9e602a6
James Conroy <jconroy12@gmail.com>
2019-01-30 00:56:42 -0500

    Game Board and Display and Population
    Vector of vector to represent the game board.
    The board is a grid provided by the user as a command line argument.
    
    The game board class has a member function to display the game board to
    the terminal.
    
    The board is randomly populated with white and black queens

--- hillClimb.h
+++ hillClimb.h
@@ -1,18 +1,4 @@
-/* This is a structure to contain the game board
- * It consists of a 2d array of intigers and an int tracking the
- * fitness of the board state
- */
 #ifndef HILLCLIMB_H
 #define HILLCLIMB_H
-
-#include <vector>
-
-class ChessBoard {
-	int fitness;
-	std::vector<std::vector<int>> board;
-
-	public:
-	ChessBoard( int row, int col);
-};
-
+void hillClimb (int row, int col, int white, int black, int tmax);
 #endif
---
+++ runScript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This program calles a program called queens multiple times for easy testing purposes
+# The script should be in the same directory as the exicutable
+
+./queens 8 8 4 4 50 test.txt hc
